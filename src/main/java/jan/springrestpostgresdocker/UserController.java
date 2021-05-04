9c0f39ae4c90e29e84491cca3741374e6479f9c4
Jan Lishak <35602700+janlishak@users.noreply.github.com>
2021-05-04 03:27:38 +0200

    ff

--- src/main/java/jan/springrestpostgresdocker/UserController.java
+++ src/main/java/jan/springrestpostgresdocker/UserController.java
@@ -16,20 +16,20 @@ public class UserController {
     this.userRepository = userRepository;
   }
 
-  @Operation(summary = "test")
+  @Operation(summary = "test 1")
   @GetMapping("/user/all")
   Iterable<UserModel> all() {
     return userRepository.findAll();
   }
 
-  @Operation(summary = "test")
+  @Operation(summary = "test 2")
   @GetMapping("/user/{id}")
   UserModel userById(@PathVariable Long id) {
     return userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(
         HttpStatus.NOT_FOUND));
   }
-  
-  @Operation(summary = "test")
+
+  @Operation(summary = "test 3")
   @PostMapping("/user/save")
   UserModel save(@RequestBody UserModel user) {
     return userRepository.save(user);
